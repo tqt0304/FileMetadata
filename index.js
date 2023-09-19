bc1f995eb950ccc6c73eea67f57f48c4e0e1bcdc
tqt0304 <tqthy034@gmail.com>
2023-09-19 22:50:23 +0700

    First

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
+const upload = multer({dest: "uploads/"})
+
 require('dotenv').config()
+
 
 var app = express();
 
@@ -11,7 +15,9 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single("upfile"), (req, res) => {
+  res.json({name: req.file.originalname, type: req.file.mimetype, size: req.file.size});
+})
 
 
 const port = process.env.PORT || 3000;
